8ae13bd0ace2544bcd33d89ca4b6a45c74aea0ef
jeremy-todd <jeremy.todd@tylertech.com>
2019-12-12 16:44:21 -0600

    Started building out Incident Report to communicate and populate with the API/db

--- USAEMS/ClientApp/src/app/Modules/IncidentReporting/incident-report/incident-report.component.ts
+++ USAEMS/ClientApp/src/app/Modules/IncidentReporting/incident-report/incident-report.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IncidentServiceService } from '../../../Services/IncidentReporting/incident-service.service';
 import { IIncident } from '../../../Interfaces/IncidentReporting/iincident';
+import { EventServiceService } from '../../../Services/Events/event-service.service';
+import { IEvent } from '../../../Interfaces/Events/ievent';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-incident-report',
@@ -9,13 +12,67 @@ import { IIncident } from '../../../Interfaces/IncidentReporting/iincident';
 })
 export class IncidentReportComponent implements OnInit {
 
-  incidentList: IIncident[] = [];
+  private incidentForm: FormGroup = new FormGroup({
+    date: new FormControl(''),
+    location: new FormControl(''),
+    incidentTime: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    addressStreet: new FormControl(''),
+    addressCity: new FormControl(''),
+    addressState: new FormControl(''),
+    addressZIP: new FormControl(''),
+    phone: new FormControl(''),
+    dateOfBirth: new FormControl(''),
+    email: new FormControl(''),
+    description: new FormControl(''),
+    employee: new FormControl(''),
+    pdCaseNumber: new FormControl(''),
+  });
 
-  constructor(private IncidentService: IncidentServiceService) { }
+  incidentList: IIncident[] = [];
+  eventList: IEvent[] = [];
+  private incidentId: Number;
+  private eventId: Number;
+
+  constructor(private IncidentService: IncidentServiceService, private EventService: EventServiceService) { }
 
   ngOnInit() {
-    this.IncidentService.getAll().subscribe(data => (this.incidentList = data));
-    console.log(this.incidentList);
+    this.IncidentService.getAll().subscribe(data => {
+      this.incidentList = data;
+      console.log(this.incidentList)
+    });
+
+    this.EventService.getAll().subscribe(data => {
+      this.eventList = data;
+      console.log(this.eventList);
+    });
+  }
+
+  onSelectEvent(event) {
+    var selectedEventId = +event.target.value;
+    if (selectedEventId > 0) {
+      var selectedEvent = this.eventList.find(e => e.id === selectedEventId);
+      this.incidentId = selectedEvent.id;
+      //console.log("this.eventId = " + this.eventId);
+    } else {
+      
+      this.incidentId = selectedEvent.id;
+      //console.log("this.eventId = " + this.eventId);
+    }
+
+  }
+
+  onSelectIncident(incident) {
+    var selectedIncidentId = +event.target.value;
+    if (selectedIncidentId > 0) {
+      var selectedIncident = this.eventList.find(e => e.id === selectedIncidentId);
+      
+      this.incidentId = selectedIncident.id;
+    } else {
+      this.incidentId = selectedIncident.id;
+    }
+
   }
 
 }
